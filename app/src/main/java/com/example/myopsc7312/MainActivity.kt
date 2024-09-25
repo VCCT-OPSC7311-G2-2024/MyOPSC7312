b61057619817d1a5259d5c2876384a2a651e22c7
UmarAD133 <161440101+UmarAD133@users.noreply.github.com>
2024-09-25 17:28:03 +0200

    Initial commit

--- app/src/main/java/com/example/myopsc7312/MainActivity.kt
+++ app/src/main/java/com/example/myopsc7312/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myopsc7312
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,5 +22,24 @@ class MainActivity : AppCompatActivity() {
         }
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener {
+            // Navigate to LoginActivity
+            val loginIntent = Intent(this, Register::class.java)
+            startActivity(loginIntent)
+        }
+
+        // Register button click listener
+        findViewById<Button>(R.id.registerButton).setOnClickListener {
+            // Navigate to RegisterActivity
+            val registerIntent = Intent(this, Login::class.java)
+            startActivity(registerIntent)
+        }
+        // Handling insets (for edge-to-edge design)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
