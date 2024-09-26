76d02db66e912171379ebae2a5618c8168d2fd45
UmarAD133 <161440101+UmarAD133@users.noreply.github.com>
2024-09-26 14:36:04 +0200

    Fifth Commit

--- app/src/main/java/com/example/myopsc7312/MainActivity.kt
+++ app/src/main/java/com/example/myopsc7312/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.example.myopsc7312
 
-import android.content.Intent
+
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
